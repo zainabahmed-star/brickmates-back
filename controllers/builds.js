582a4c4cc9901034b50ccd285d2fb178f14c534c
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-27 11:00:48 +0300

    adding the index+show functions and fixing some errors

--- controllers/builds.js
+++ controllers/builds.js
@@ -1,6 +1,6 @@
 const cloudinary = require('../config/cloudinary');
 const Build = require('../models/build');
-
+const axios = require('axios');
 
 const uploadMedia = (fileBuffer, resourceType) => {
   return new Promise((resolve, reject) => {
@@ -23,12 +23,13 @@ const uploadMedia = (fileBuffer, resourceType) => {
 };
 
 const create = async (req, res) => {
- 
   try {
-    const ownerId = req.user._id
+    const ownerId = req.user_id    
+
     const set = await axios.get(`https://rebrickable.com/api/v3/lego/sets/${req.body.setNum}/`, {
     headers: { Authorization: `key ${process.env.REBRICKABLE_API_KEY}` }
     })
+    
     let image = {}
 
     if (req.file) {
@@ -64,6 +65,33 @@ const create = async (req, res) => {
   }
 }
 
+const index = async (req, res) => {
+  try {
+    const build = await Build.find().populate("owner").sort({ createdAt: -1 })
+    
+    res.status(200).json(build)
+  } catch (error) {
+    res.status(500).json({ err: error.message })
+  }
+}
+
+
+const show = async (req, res) => {
+  try {
+    const build = await Build.findById(req.params.buildId).populate('owner')
+
+    if (!build) {
+      return res.status(404).json({ err: 'Build not found' })
+    }
+
+    res.status(200).json(build)
+  } catch (err) {
+    res.status(500).json({ err: err.message })
+  }
+}
+
 module.exports ={
     create,
+    index,
+    show,
 }
